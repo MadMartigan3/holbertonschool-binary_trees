c8b2d0c28ac70e81706d34ea270ce16d6bce8a02
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-08 11:53:14 +0100

    creation de noeud

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @node: node create
+ * Return: return node
+ * 
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node = malloc(sizeof(*node));
+    if (node == NULL)
+    {
+        printf(stderr, "Erreur allocation de memoire");
+        exit(EXIT_FAILURE);
+    }
+
+    node->n = value;
+    node->left = NULL;
+    node->right = NULL;
+    node->parent = NULL;
+
+    printf(" création de node %d", node->n);
+
+    return(node);
+}
