1c778215f423e1b71708e76b3db6f2ed774791e4
root <root@LAPTOP-2LV4F0PM>
2024-01-09 13:57:37 +0100

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * 
-*/
+ *
+ */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    
+    size_t left_height, right_height;
+
+    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+    {
+        return (0);
+    }
+
+    left_height = binary_tree_depth(tree->left);
+    right_height = binary_tree_depth(tree->right);
+
+    if (left_height >= right_height)
+    {
+        return left_height + 1;
+    }
+    else
+        return right_height + 1;
 }
