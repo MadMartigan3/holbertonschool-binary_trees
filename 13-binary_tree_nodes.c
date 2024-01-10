44056b3ad44f051d7dd8d848a4418b4181d654f0
root <root@LAPTOP-2LV4F0PM>
2024-01-10 10:53:06 +0100

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,20 @@
 #include "binary_trees.h"
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    
+    size_t count_nodes;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    if (tree->left == NULL && tree->right == NULL)
+    {
+        return (0);
+    }
+
+    count_nodes = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
+
+    return (count_nodes);
 }
