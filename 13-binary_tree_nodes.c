9aa47166e360d7a7cdf9bf2cc19a36d3b8eeeddc
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-11 15:16:28 +0100

    betty correction

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,7 +9,7 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t count_nodes;
+	size_t count;
 
 	if (tree == NULL)
 	{
@@ -21,7 +21,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	count_nodes = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
+	count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
 
-	return (count_nodes);
+	return (count);
 }
