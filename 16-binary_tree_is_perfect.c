1e8051506d1d6dc2f1916a18c956b84686483756
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-11 13:36:22 +0100

    question 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_height - function that print the height
  * of a tree
@@ -37,13 +36,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+	
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
 	}
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height == right_height)
 	{
