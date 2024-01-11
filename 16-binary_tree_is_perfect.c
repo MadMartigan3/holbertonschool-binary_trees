e2f7bcef5538d5e5ca547f4b6701dfa6b051fd96
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-11 13:45:37 +0100

    Correction Question 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,10 +36,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t left_height, right_height;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 	{
 		return (0);
 	}
@@ -49,13 +48,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (left_height == right_height)
 	{
-		if (tree->left == NULL && tree->right == NULL)
-		{
-			return (1);
-		}
-
-		if (tree->left && tree->right && binary_tree_is_perfect(tree->left)
-		&& binary_tree_is_perfect(tree->right))
+		if ((tree->left == NULL && tree->right == NULL) ||
+		    (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right)))
 		{
 			return (1);
 		}
