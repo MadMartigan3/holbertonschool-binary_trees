d5cf516eef4ed844090e6a789aec30044a206290
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-11 11:23:31 +0100

    correctio question 1/2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,7 +10,7 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *rightnode = malloc(sizeof(*rightnode));
+	binary_tree_t *rightnode = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL)
 	{
