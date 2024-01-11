8063fb577763de4569ed4346cb3beec4ab2e90a8
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-11 14:35:08 +0100

    betty correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,12 +10,13 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *leftnode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *leftnode;
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
+	leftnode = malloc(sizeof(binary_tree_t));
 	if (leftnode == NULL)
 	{
 		return (NULL);
