e93a4e2d85ab3660d7fc4220b43cd7f3eaba4666
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-11 11:29:40 +0100

    binary_tree_full.c modified

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,18 +9,10 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-	return (0);
+		return (0);
 
-	if (tree->left && tree->right)
-	{
-		if ((tree->left->left) == NULL || (tree->right->right) == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-		else
-		return (0);
-	}
-
-	else
-	return (0);
-
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
