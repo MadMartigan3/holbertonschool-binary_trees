ed0b898f9222be6f4d13590a3d3527563e939faf
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-11 14:52:33 +0100

    betty correction

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,16 +6,15 @@
 */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent)
+	if (node == NULL)
 	{
 		return (0);
 	}
-	else
+	if (node->parent == NULL)
 	{
 		return (1);
 	}
-
-	if (node == NULL)
+	else
 	{
 		return (0);
 	}
